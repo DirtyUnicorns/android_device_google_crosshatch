b5b02fd30f96bf8853d1b9bbd8df92e24a0a0e60
Josh Chasky <jchasky@gmail.com>
2018-10-22 19:21:58 -0400

    DU-ify!

---
+++ du.mk
@@ -0,0 +1,46 @@
+# Copyright (C) 2018 The Dirty Unicorns Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for an aosp Android
+# build for marlin hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps). Except for a few implementation
+# details, it only fundamentally contains two inherit-product
+# lines, aosp and du, hence its name.
+#
+
+# Include DU common configuration
+include vendor/du/config/common_full_phone.mk
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/google/crosshatch/aosp_crosshatch.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+PRODUCT_NAME := du_crosshatch
+PRODUCT_DEVICE := crosshatch
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Pixel 3 XL
+PRODUCT_MANUFACTURER := Google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=crosshatch \
+    BUILD_FINGERPRINT=google/crosshatch/crosshatch:9/PD1A.180720.030/4972053:user/release-keys \
+    PRIVATE_BUILD_DESC="crosshatch-user 9 PD1A.180720.030 4972053 release-keys"
+
+$(call inherit-product-if-exists, vendor/google/crosshatch/crosshatch-vendor.mk)
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
