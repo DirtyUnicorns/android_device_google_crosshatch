907db5a49afb396b324cfa6d49855b50ad9d061a
Nathan Chancellor <nathan@dirtyunicorns.com>
2018-10-22 12:56:33 -0400

    Allow kernel to compile inline
    * Do not use prebuilt kernel

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -42,6 +42,12 @@ BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem
 # STOPSHIP Bringup hack- no low power
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := 7.0.2
+TARGET_KERNEL_SOURCE := kernel/google/bluecross
+TARGET_KERNEL_CONFIG := b1c1_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
