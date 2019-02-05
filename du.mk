951e10edf21a1f4f52ca13b9f247435c59ac167e
Josh Chasky <jchasky@gmail.com>
2019-02-05 14:19:24 -0500

    Update build fingerprint to PQ2A.190205.001
    Change-Id: I588c4025d2d5d10b8b5ab1e1689ea82697d8f545

--- du.mk
+++ du.mk
@@ -39,8 +39,8 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=crosshatch \
-    BUILD_FINGERPRINT=google/crosshatch/crosshatch:9/PQ1A.190105.004/5148680:user/release-keys \
-    PRIVATE_BUILD_DESC="crosshatch-user 9 PQ1A.190105.004 5148680 release-keys"
+    BUILD_FINGERPRINT=google/crosshatch/crosshatch:9/PQ2A.190205.001/5163636:user/release-keys \
+    PRIVATE_BUILD_DESC="crosshatch-user 9 PQ2A.190205.001 5163636 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/crosshatch/crosshatch-vendor.mk)
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
