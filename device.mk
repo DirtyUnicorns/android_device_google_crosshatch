eb03063785e240b032d10f9b3cd32789a1bbf43e
Josh Chasky <jchasky@gmail.com>
2019-09-13 15:21:51 -0400

    Remove duplicate privapp permissions prop
    * This is already in vendor as enforce, no need to list it here.
    
    Change-Id: I76fddb4e74855c968bb62ff50898d990ae8c8756

--- device.mk
+++ device.mk
@@ -42,8 +42,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.verified_boot.xml
 
 # Enforce privapp-permissions whitelist
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.control_privapp_permissions=enforce
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.control_privapp_permissions=enforce
 
 # Enable on-access verification of priv apps. This requires fs-verity support in kernel.
 PRODUCT_PRODUCT_PROPERTIES += \
