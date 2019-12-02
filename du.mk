9b00fb3798d013761f88c61d3babf6f54ee10b6e
Josh Chasky <jchasky@gmail.com>
2019-12-02 15:47:00 -0500

    Update build fingerprint to QQ1A.191205.008
    Change-Id: Ibf808a01d3f52edab922d2c1aff9bfadf606a311

--- du.mk
+++ du.mk
@@ -40,12 +40,12 @@ PRODUCT_MANUFACTURER := Google
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="crosshatch" \
     TARGET_DEVICE="crosshatch" \
-    PRIVATE_BUILD_DESC="crosshatch-user 10 QP1A.191105.003 5899767 release-keys"
+    PRIVATE_BUILD_DESC="crosshatch-user 10 QQ1A.191205.008 5974827 release-keys"
 
-BUILD_FINGERPRINT := "google/crosshatch/crosshatch:10/QP1A.191105.003/5899767:user/release-keys"
+BUILD_FINGERPRINT := "google/crosshatch/crosshatch:10/QQ1A.191205.008/5974827:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/crosshatch/crosshatch:10/QP1A.191105.003/5899767:user/release-keys
+    ro.build.fingerprint=google/crosshatch/crosshatch:10/QQ1A.191205.008/5974827:user/release-keys
 
 BOARD_PREBUILT_VENDORIMAGE := vendor/images/crosshatch/vendor.img
 
