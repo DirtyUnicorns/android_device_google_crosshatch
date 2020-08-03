a9ea6fd78697d99d5e30c9d7844126d7e97b2275
Josh Chasky <jchasky@gmail.com>
2020-08-03 17:12:26 -0400

    Update build fingerprint to QQ3A.200805.001
    Change-Id: Ia546c4133f1582e53ba4b23582ecc7adae4ebb17

--- du.mk
+++ du.mk
@@ -38,12 +38,12 @@ PRODUCT_MODEL := Pixel 3 XL
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="crosshatch" \
-    PRIVATE_BUILD_DESC="crosshatch-user 10 QQ3A.200705.002 6506677 release-keys"
+    PRIVATE_BUILD_DESC="crosshatch-user 10 QQ3A.200805.001 6578210 release-keys"
 
-BUILD_FINGERPRINT := "google/crosshatch/crosshatch:10/QQ3A.200705.002/6506677:user/release-keys"
+BUILD_FINGERPRINT := "google/crosshatch/crosshatch:10/QQ3A.200805.001/6578210:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/crosshatch/crosshatch:10/QQ3A.200705.002/6506677:user/release-keys
+    ro.build.fingerprint=google/crosshatch/crosshatch:10/QQ3A.200805.001/6578210:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/crosshatch/crosshatch-vendor.mk)
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
