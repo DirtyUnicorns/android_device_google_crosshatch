16ef08a5c5ff3edcab2343374f90f6c7c5fdc04b
Ethan Chen <intervigil@gmail.com>
2020-09-15 16:32:52 -0400

    Declare EUICC permission
    Change-Id: I1b151021ca9fc6cf35141b2ccb337bc83197089e

--- device.mk
+++ device.mk
@@ -907,3 +907,7 @@ PRODUCT_PACKAGES += \
     libjson \
     libtinyxml \
     libwifi-hal-qcom
+
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
