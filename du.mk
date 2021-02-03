756f3578c6718158c60070108b8d8478c6653598
Josh Chasky <jchasky@gmail.com>
2021-02-03 12:55:02 -0500

    Update build fingerprint to RQ1A.210205.004
    Change-Id: I8f2740a887782c6042417db684efaef821453181

--- du.mk
+++ du.mk
@@ -38,12 +38,12 @@ PRODUCT_MODEL := Pixel 3 XL
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="crosshatch" \
-    PRIVATE_BUILD_DESC="crosshatch-user 11 RQ1A.210105.003 7005429 release-keys"
+    PRIVATE_BUILD_DESC="crosshatch-user 11 RQ1A.210205.004 7038034 release-keys"
 
-BUILD_FINGERPRINT := "google/crosshatch/crosshatch:11/RQ1A.210105.003/7005429:user/release-keys"
+BUILD_FINGERPRINT := "google/crosshatch/crosshatch:11/RQ1A.210205.004/7038034:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/crosshatch/crosshatch:11/RQ1A.210105.003/7005429:user/release-keys
+    ro.build.fingerprint=google/crosshatch/crosshatch:11/RQ1A.210205.004/7038034:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/crosshatch/crosshatch-vendor.mk)
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
